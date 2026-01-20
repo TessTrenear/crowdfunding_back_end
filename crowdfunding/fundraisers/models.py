a0236a8a73ccb355d324dadfb1e12b43e0c880e3
Tess <tesstrenear@outlook.com>
2026-01-20 20:27:41 +1100

    created fundraisers and pledges

--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -8,3 +8,13 @@ class Fundraiser(models.Model):
     image = models.URLField() # URLS > address of photo, not storing image in DB directly, only store URL. Don't store images in DB, store images in other location
     is_open = models.BooleanField() # true or false
     date_created = models.DateTimeField(auto_now_add=True) #contain date and time > auto_now_add = sets the field for you
+
+class Pledge(models.Model):
+    amount = models.IntegerField()
+    comment = models.CharField(max_length=200)
+    anonymous = models.BooleanField()
+    fundraiser = models.ForeignKey(
+        'Fundraiser', #send name of Class Foreign key is on. Auto user primary key for Fundraiser class.
+        on_delete=models.CASCADE, #what to do DELETE > on delete of a FUNDRAISER cascade > finds all pledges to all that fundraiser and delete the pledges. Stops bad data.
+        related_name='pledges' #special property allow you to access all pledges for that fundraiser > by using a special property. Don't need to query the pledges table, DJANGO will do it on our behalf
+    )   
