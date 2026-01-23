aa4edbbc89cee0adbcad40b22651a23706fa035c
Tess <tesstrenear@outlook.com>
2026-01-24 10:44:22 +1100

    views and serializers updated

--- crowdfunding/fundraisers/serializers.py
+++ crowdfunding/fundraisers/serializers.py
@@ -10,3 +10,6 @@ class PledgeSerializer(serializers.ModelSerializer):
     class Meta:
         model = apps.get_model('fundraisers.Pledge')
         fields = '__all__'
+
+class FundraiserDetailSerializer(FundraiserSerializer) : #Inherit FundraiserSerializer, do not need to repeat code. Only add the difference.
+    pledges = PledgeSerializer(many=True, read_only=True)
